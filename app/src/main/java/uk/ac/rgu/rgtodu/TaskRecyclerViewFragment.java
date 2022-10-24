5cc914f20185d3fd1b6b629b5f9451ded4593f66
dcorsar <20Pfgh!19>
2022-10-25 00:40:33 +0100

    Added navigation between home and other fragments.

--- app/src/main/java/uk/ac/rgu/rgtodu/TaskRecyclerViewFragment.java
+++ app/src/main/java/uk/ac/rgu/rgtodu/TaskRecyclerViewFragment.java
@@ -10,10 +10,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link TaskRecyclerViewActivity#newInstance} factory method to
+ * Use the {@link TaskRecyclerViewFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class TaskRecyclerViewActivity extends Fragment {
+public class TaskRecyclerViewFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +24,7 @@ public class TaskRecyclerViewActivity extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public TaskRecyclerViewActivity() {
+    public TaskRecyclerViewFragment() {
         // Required empty public constructor
     }
 
@@ -37,8 +37,8 @@ public class TaskRecyclerViewActivity extends Fragment {
      * @return A new instance of fragment TaskListRecyclerViewActivity.
      */
     // TODO: Rename and change types and number of parameters
-    public static TaskRecyclerViewActivity newInstance(String param1, String param2) {
-        TaskRecyclerViewActivity fragment = new TaskRecyclerViewActivity();
+    public static TaskRecyclerViewFragment newInstance(String param1, String param2) {
+        TaskRecyclerViewFragment fragment = new TaskRecyclerViewFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
